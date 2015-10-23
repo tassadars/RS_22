135515ab7a8a3bd46225b17b511209466981c3e4
eugen <tassadars@gmail.com>
2015-10-23 18:45:18 +0300

    Add pages about, visit, contacts

--- app.rb
+++ app.rb
@@ -24,6 +24,20 @@ get '/' do
   erb 'Can you handle a <a href="/secure/place">secret</a>?'
 end
 
+get '/about' do
+  erb :about
+end
+
+get '/visit' do
+  erb :visit
+end
+
+get '/contacts' do
+  erb :contacts
+end
+
+
+
 get '/login/form' do
   erb :login_form
 end
